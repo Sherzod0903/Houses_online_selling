3bc418b2339594bb62828bc6b2ad065440e7f347
Sherzod0903 <sherzod.sh171@gmail.com>
2022-11-29 21:17:30 +0900

    feature added

---
+++ src/components/HouseItem/index.jsx
@@ -0,0 +1,27 @@
+import React,{ }from "react";
+
+import {  Container ,Squares,Icons} from "./styled";
+
+ 
+export const Houseitem=()=>{ 
+
+
+return (
+  <div>HouseItem</div>
+)
+};
+export default Houseitem;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
